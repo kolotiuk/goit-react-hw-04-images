b0e52da62e15d2d42157e2376226b77fe4b6f9be
Ihor <ihorfront88@gmail.com>
2022-10-11 15:55:44 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Modal from './Modal/Modal';
+import { getImagesApi } from 'utils/imagesApi';
 
 const App = () => {
   const [query, setQuery] = useState('');
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalImg, setModalImg] = useState(null);
+  const [imagesGallery, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const onSubmit = query => {
-    setQuery(query);
+  const onSubmit = q => {
+    setQuery(prev => {
+      if (prev !== q) {
+        setImages([]);
+        setPage(1);
+      }
+    });
+    setQuery(q);
   };
 
   const toggleModal = (modalImage = null) => {
@@ -17,10 +28,47 @@ const App = () => {
     setModalImg(modalImage);
   };
 
+  useEffect(() => {
+    if (query === '') {
+      setError(new Error('Enter to search images!'));
+    } else {
+      getImages();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page, query]);
+
+  const getImages = () => {
+    setIsLoading(true);
+    setError(null);
+
+    getImagesApi(query, page)
+      .then(images => {
+        if (!images.length) {
+          throw new Error('Bad request!!!');
+        }
+        setImages(prev => [...prev, ...images]);
+      })
+      .catch(error => {
+        setError(error);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  const handleLoadMoreImages = () => {
+    setPage(prev => prev + 1);
+  };
+
   return (
     <div className="App">
       <Searchbar onSubmit={onSubmit} />
-      <ImageGallery query={query} toggleModal={toggleModal} />
+      <ImageGallery
+        query={query}
+        toggleModal={toggleModal}
+        imagesGallery={imagesGallery}
+        handleLoadMoreImages={handleLoadMoreImages}
+        isLoading={isLoading}
+        error={error}
+      />
       {isModalOpen && <Modal modalImg={modalImg} closeModal={toggleModal} />}
     </div>
   );
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,60 +1,16 @@
-import { useState, useEffect } from 'react';
 import Button from 'components/Button';
 import ImageGalleryItem from 'components/ImageGalleryItem';
-import { getImagesApi } from './../../utils/imagesApi';
 import Loader from './../Loader/Loader';
 import s from './ImageGallery.module.scss';
 import { PropTypes } from 'prop-types';
 
-const ImageGallery = ({ query, toggleModal }) => {
-  const [imagesGallery, setImages] = useState([]);
-  const [page, setPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  // static propTypes = {
-  //   query: PropTypes.string.isRequired,
-  //   toggleModal: PropTypes.func.isRequired,
-  // };
-
-  useEffect(() => {
-    setImages([]);
-    setPage(1);
-  }, []);
-
-  useEffect(() => {
-    // setImages([]);
-    // setPage(1);
-    if (query === '') {
-      setError(new Error('Enter to search images!'));
-    } else {
-      // setPage(1);
-      getImages();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page, query]);
-
-  const getImages = () => {
-    setIsLoading(true);
-    setError(null);
-
-    getImagesApi(query, page)
-      .then(images => {
-        if (!images.length) {
-          throw new Error('Bad request!!!');
-        }
-        setImages(prev => [...prev, ...images]);
-      })
-      .catch(error => {
-        setError(error);
-      })
-      .finally(() => setIsLoading(false));
-  };
-
-  const handleLoadMoreImages = () => {
-    setPage(page + 1);
-  };
-
+const ImageGallery = ({
+  toggleModal,
+  imagesGallery,
+  handleLoadMoreImages,
+  isLoading,
+  error,
+}) => {
   return (
     <>
       <ul className={s.ImageGallery}>
@@ -79,82 +35,19 @@ const ImageGallery = ({ query, toggleModal }) => {
     </>
   );
 };
-// class ImageGallery extends Component {
-//   state = {
-//     images: [],
-//     page: 1,
-//     isLoading: false,
-//     error: null,
-//   };
-
-//   static propTypes = {
-//     query: PropTypes.string.isRequired,
-//     toggleModal: PropTypes.func.isRequired,
-//   };
-
-//   componentDidUpdate(prevProps, prevState) {
-//     if (prevProps.query !== this.props.query) {
-//       this.setState({ images: [], page: 1 });
-//     }
-//     if (
-//       prevState.page !== this.state.page ||
-//       (prevProps.query !== this.props.query && this.state.page === 1)
-//     ) {
-//       if (this.props.query === '') {
-//         this.setState({ error: new Error('Value NONE!!!') });
-//       } else {
-//         this.getImages();
-//       }
-//     }
-//   }
-
-//   getImages = () => {
-//     this.setState({ isLoading: true, error: null });
-//     getImagesApi(this.props.query, this.state.page)
-//       .then(images => {
-//         if (!images.length) {
-//           throw new Error('Bad request!!!');
-//         }
-//         this.setState(prev => ({ images: [...prev.images, ...images] }));
-//       })
-//       .catch(error => {
-//         this.setState({ error });
-//       })
-//       .finally(() => this.setState({ isLoading: false }));
-//   };
-
-//   handleLoadMoreImages = () => {
-//     this.setState(prev => ({ page: prev.page + 1 }));
-//   };
-
-//   render() {
-//     const { images, isLoading, error } = this.state;
-//     const { toggleModal } = this.props;
-
-//     return (
-//       <>
-//         <ul className={s.ImageGallery}>
-//           {images.map(({ id, webformatURL, largeImageURL }) => {
-//             return (
-//               <ImageGalleryItem
-//                 key={id}
-//                 id={id}
-//                 webformatURL={webformatURL}
-//                 largeImageURL={largeImageURL}
-//                 openModal={toggleModal}
-//               />
-//             );
-//           })}
-//         </ul>
-//         {images.length > 0 &&
-//           (images.length < 12 || (
-//             <Button handleLoadMoreImages={this.handleLoadMoreImages} />
-//           ))}
-//         {isLoading && <Loader />}
-//         {error && <h1>{error.message}</h1>}
-//       </>
-//     );
-//   }
-// }
 
 export default ImageGallery;
+
+ImageGallery.propTypes = {
+  toggleModal: PropTypes.func.isRequired,
+  handleLoadMoreImages: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  error: PropTypes.object,
+  imagesGallery: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      webformatURL: PropTypes.string.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
+    })
+  ),
+};
